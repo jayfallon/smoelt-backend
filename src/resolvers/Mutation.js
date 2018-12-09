fdea407c45bb0ee00c3e4b8b6d81fc67a42ffa18
jf1016613 <james_fallon@bose.com>
2018-12-09 16:25:30 -0500

    added user functionality, hash, permissions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		// check if they are logged in
@@ -35,6 +37,32 @@ const Mutations = {
 		//check if they own it or has permissions
 		// delete
 		return ctx.db.mutation.deleteItem({ where }, info);
+	},
+	async signup(parent, args, ctx, info) {
+		// always lowercase the email
+		args.email = args.email.toLowerCase();
+		// hash the password
+		const password = await bcrypt.hash(args.password, 13);
+		// create the user in the db
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: ["USER"] },
+				},
+			},
+			info
+		);
+		// create JWT token
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// set the jwt as a cookie on the response
+		ctx.response.cookie("token", token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+		});
+		// return the user
+		return user;
 	},
 };
 
