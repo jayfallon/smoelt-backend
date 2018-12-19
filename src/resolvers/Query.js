03a94a0da2a570380078aab6cb15f1c1030b0454
jf1016613 <james_fallon@bose.com>
2018-12-18 21:28:26 -0500

    added single orders to schema

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -27,6 +27,27 @@ const Query = {
 		// if they do, query all the users
 		return ctx.db.query.users({}, info);
 	},
+	async order(parent, args, ctx, info) {
+		//1. ,make sure they are logged in
+		if (!ctx.request.userId) {
+			throw new Error("You aren't logged in!");
+		}
+		//2. Query the current order
+		const order = await ctx.db.query.order(
+			{
+				where: { id: args.id },
+			},
+			info
+		);
+		//3. Check if they have permissions to see this order
+		const ownsOrder = order.user.id === ctx.request.userId;
+		const hasPermissionToSeeOrder = ctx.request.user.permissions.includes("ADMIN");
+		if (!ownsOrder || !hasPermissionToSeeOrder) {
+			throw new Error("You need permission to see this.");
+		}
+		//4. return the order
+		return order;
+	},
 };
 
 module.exports = Query;
