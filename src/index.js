ca85c8c4c4e941530ee22da5b41f7752b2ee03b9
jf1016613 <james_fallon@bose.com>
2018-12-21 13:47:07 -0500

    put the cors back in

--- src/index.js
+++ src/index.js
@@ -31,12 +31,12 @@ server.express.use(async (req, res, next) => {
 
 // start the server
 server.start(
-	// {
-	// 	cors: {
-	// 		credentials: true,
-	// 		origin: process.env.FRONTEND_URL,
-	// 	},
-	// },
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		},
+	},
 	details => {
 		console.log(`Server is now running on port http:/localhost:${details.port}`);
 	}
